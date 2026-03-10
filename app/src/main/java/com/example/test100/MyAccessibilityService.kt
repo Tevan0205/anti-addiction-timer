ae86a07bffc4a1e12d069bef84b71c4399e56e5b
Tuna_Canned <tung05200219@gmail.com>
2026-03-11 01:51:56 +0800

    v0.24 add warning timer and limit timer

--- app/src/main/java/com/example/test100/MyAccessibilityService.kt
+++ app/src/main/java/com/example/test100/MyAccessibilityService.kt
@@ -1,9 +1,18 @@
 package com.example.test100
 
 import android.accessibilityservice.AccessibilityService
+import android.content.Intent
 import android.view.accessibility.AccessibilityEvent
+import android.widget.Toast
 
 class MyAccessibilityService : AccessibilityService() {
+
+    private var seconds = 0
+
+    private val warningTime = 5
+    private val limitTime = 10
+
+    private var lastApp = ""
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
 
@@ -19,14 +28,55 @@ class MyAccessibilityService : AccessibilityService() {
                 MODE_PRIVATE
             )
 
+        val selected =
+            prefs.getStringSet(
+                "apps",
+                emptySet()
+            ) ?: emptySet()
+
+        if (pkg != lastApp) {
+            seconds = 0
+            lastApp = pkg
+        }
+
         prefs.edit()
             .putString(
                 "currentApp",
                 pkg
             )
             .apply()
+
+        if (!selected.contains(pkg)) return
+
+        seconds++
+
+        if (seconds == warningTime) {
+
+            Toast.makeText(
+                this,
+                "快到時間了",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+
+        if (seconds >= limitTime) {
+
+            val home =
+                Intent(Intent.ACTION_MAIN)
+
+            home.addCategory(
+                Intent.CATEGORY_HOME
+            )
+
+            home.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK
+
+            startActivity(home)
+
+        }
+
     }
 
     override fun onInterrupt() {}
-
 }
